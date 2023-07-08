7d31dd124e56f8f410eea975b0770a085092b93e
asimmujahid <asimmujahid2018@gmail.com>
2023-07-08 15:32:10 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp(
+    child: MaterialApp(
+      title: "Drivers App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //home: MySplash(),
+    ),
+  )
+  );
+}
+
+// class MyApp extends StatefulWidget {
+//   final Widget? child;
+//   MyApp({this.child});
+
+//     static void restartApp(BuildContext context) {
+//     context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return 
+//   }
+class MyApp extends StatefulWidget {
+  final Widget? child;
+  MyApp({this.child});
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
+  // const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Key key = UniqueKey();
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child!,
+    );
+  }
+}
+
+
